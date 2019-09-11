e66c585a4cb57659b0afcef7ac03a20441b06a35
MKMX <maxo5@yonyou.com>
2019-09-11 21:31:10 +0800

    初始化，mock,axios,elementui等

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/views/HelloWorld'
+import Login from "@/views/Login"
+import Home from "@/views/Home"
+import NotFound from "@/views/404"
 
 Vue.use(Router)
 
@@ -8,8 +11,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/Login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/404',
+      name: 'notFound',
+      component: NotFound
     }
   ]
 })
